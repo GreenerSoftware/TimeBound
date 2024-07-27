c973a0886c7d531f7bee9f508c6c56a94a39813f
David Carboni <david@carboni.io>
2024-07-27 14:08:40 +0100

    Scheduled shutdown lambda

---
+++ shutdown/src/lambda.ts
@@ -0,0 +1,21 @@
+// https://aws.amazon.com/blogs/mobile/understanding-amazon-cognito-user-pool-oauth-2-0-grants/
+import {
+  ScheduledEvent,
+} from 'aws-lambda';
+import { AutoScalingClient, SetDesiredCapacityCommand } from "@aws-sdk/client-auto-scaling";
+
+const client = new AutoScalingClient();
+
+export async function handler(event: ScheduledEvent): Promise<any> {
+  console.log(JSON.stringify(event, null, 2));
+
+  // Shut down instance
+  const command = new SetDesiredCapacityCommand({
+    AutoScalingGroupName: process.env.AUTO_SCALING_GROUP_NAME,
+    DesiredCapacity: 0,
+  });
+  const data = await client.send(command);
+  console.log(JSON.stringify(data));
+
+  return 'ok';
+}
