a1e6bc120444c69a74c521a33d3dcda3f7556c7a
David Carboni <david@carboni.io>
2024-07-27 15:45:50 +0100

    Add RDS code

--- startup/src/lambda.ts
+++ startup/src/lambda.ts
@@ -3,23 +3,29 @@ import {
   ScheduledEvent,
 } from 'aws-lambda';
 import { AutoScalingClient, UpdateAutoScalingGroupCommand } from "@aws-sdk/client-auto-scaling";
+import { RDSClient, StartDBInstanceCommand } from "@aws-sdk/client-rds";
 
-const client = new AutoScalingClient();
+const autoScalingClient = new AutoScalingClient();
+const rdsClient = new RDSClient();
+
 
 export async function handler(event: ScheduledEvent): Promise<void> {
   console.log(JSON.stringify(event, null, 2));
 
-  // Shut down instance
-  const command = new UpdateAutoScalingGroupCommand({
+  // Shut down ec2
+  const asgCommand = new UpdateAutoScalingGroupCommand({
     AutoScalingGroupName: process.env.AUTO_SCALING_GROUP_NAME,
     MinSize: 1,
     MaxSize: 1,
     DesiredCapacity: 1,
   });
-  // const command = new SetDesiredCapacityCommand({
-  //   AutoScalingGroupName: process.env.AUTO_SCALING_GROUP_NAME,
-  //   DesiredCapacity: 1,
-  // });
-  const data = await client.send(command);
+  const data = await autoScalingClient.send(asgCommand);
+
+  // stop rds
+  const rdsComand = new StartDBInstanceCommand({
+    DBInstanceIdentifier: process.env.RDS_INSTANCE_NAME,
+  });
+  rdsClient.send(rdsComand);
+
   console.log(JSON.stringify(data));
 }
